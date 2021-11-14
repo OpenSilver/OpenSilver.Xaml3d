57543261a1f1733278aa4f1e9ff1f92a76bd6e09
giovanni-a <8248552+giovanni-a@users.noreply.github.com>
2021-11-14 13:23:30 +0100

    Project structure

---
+++ src/OpenSilver.Xaml3d.Demo.Browser/Pages/Index.cs
@@ -0,0 +1,26 @@
+﻿using DotNetForHtml5;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+using Microsoft.JSInterop;
+using OpenSilver.Xaml3d.Demo.Browser.Interop;
+
+namespace OpenSilver.Xaml3d.Demo.Browser.Pages
+{
+    [Route("/")]
+    public class Index : ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder __builder)
+        {
+        }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            Cshtml5Initializer.Initialize(new UnmarshalledJavaScriptExecutionHandler(JSRuntime));
+            Program.RunApplication();
+        }
+
+        [Inject]
+        private IJSRuntime JSRuntime { get; set; }
+    }
+}
