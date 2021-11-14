f407e61b4a19bd4fc4a9ffad3046de2a2b3147f5
giovanni-a <8248552+giovanni-a@users.noreply.github.com>
2021-11-14 17:51:41 +0100

    Add ObjectPosition class

---
+++ src/OpenSilver.Xaml3d/ObjectPosition.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+
+namespace OpenSilver.Xaml3d
+{
+    public static class ObjectPosition
+    {
+        public static double GetRotateX(DependencyObject obj)
+        {
+            return (double)obj.GetValue(AriaLabelProperty);
+        }
+
+        public static void SetRotateX(DependencyObject obj, double value)
+        {
+            obj.SetValue(AriaLabelProperty, value);
+        }
+
+        public static readonly DependencyProperty AriaLabelProperty =
+            DependencyProperty.RegisterAttached(
+                name: "RotateX",
+                propertyType: typeof(double),
+                ownerType: typeof(ObjectPosition),
+                typeMetadata: new PropertyMetadata(defaultValue: null)
+                {
+                    MethodToUpdateDom = (d, newValue) => { Update(); }
+                });
+
+        private static void Update()
+        {
+
+        }
+    }
+}
